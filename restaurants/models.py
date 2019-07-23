e3101390225d3f3da55bacd4f050545e1c5a56ce
Bandar SEBAA <bandar@Bandars-MacBook-Pro.local>
2019-07-23 15:50:39 +0300

    add field

--- restaurants/models.py
+++ restaurants/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Restaurant(models.Model):
 
     name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150)
     description = models.TextField()
     opening_time = models.TimeField()
     closing_time = models.TimeField()
